e24226413562f7eb5e4f598d3fe11c383249bb3c
hirrao <git@hirrao.com>
2025-04-12 00:59:26 +0800

    refactor

--- app/src/main/java/com/hirrao/appktp/display/InputField.kt
+++ app/src/main/java/com/hirrao/appktp/display/InputField.kt
@@ -11,6 +11,9 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableDoubleState
+import androidx.compose.runtime.MutableIntState
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableDoubleStateOf
 import androidx.compose.runtime.mutableIntStateOf
@@ -30,15 +33,11 @@ import com.hirrao.appktp.theme.AppTheme
 fun CommonInputField(
     modifier: Modifier = Modifier
 ) {
-    var id by remember { mutableStateOf("") }
-    var name by remember { mutableStateOf("") }
-    var age by remember { mutableIntStateOf(0) }
-    var height by remember { mutableDoubleStateOf(0.0) }
-    var ageText by remember { mutableStateOf("") }
-    var heightText by remember { mutableStateOf("") }
-    var showDialog by remember { mutableStateOf(false) }
-    var ageError by remember { mutableStateOf(false) }
-    var heightError by remember { mutableStateOf(false) }
+    var id = remember { mutableIntStateOf(0) }
+    var name = remember { mutableStateOf("") }
+    var age = remember { mutableIntStateOf(0) }
+    var height = remember { mutableDoubleStateOf(0.0) }
+    var showDialog = remember { mutableStateOf(false) }
     Column(
         modifier = Modifier
             .padding(24.dp)
@@ -46,53 +45,78 @@ fun CommonInputField(
             .then(modifier),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        val textModifier = Modifier
-            .padding(8.dp)
-            .height(72.dp)
-            .fillMaxWidth()
-        TextField(modifier = textModifier, value = id, onValueChange = {
-            id = it
-        }, label = { Text("ID") })
-        TextField(modifier = textModifier, value = name, onValueChange = {
-            name = it
-        }, label = { Text("Name") })
-        TextField(modifier = textModifier, value = ageText, isError = ageError, supportingText = {
-            if (ageError) {
-                Text("请输入正确的年龄")
-            }
-        }, onValueChange = {
-            ageText = it
-            ageError = !ageText.isDigitsOnly()
-            age = ageText.toIntOrNull() ?: 0
-        }, label = { Text("Age") })
-        TextField(
-            modifier = textModifier,
-            value = heightText,
-            isError = heightError,
-            supportingText = {
-                if (heightError) {
-                    Text("请输入正确的身高")
-                }
-            },
-            onValueChange = {
-                heightText = it
-                heightError = !heightText.isDigitsOnly()
-                height = heightText.toDoubleOrNull() ?: 0.0
-            },
-            label = { Text("Height") })
-        Row(
-            horizontalArrangement = Arrangement.Center
-        ) {
-            val buttonModifier = Modifier
-                .padding(16.dp)
-                .width(128.dp)
-                .height(54.dp)
-            Button(modifier = buttonModifier, onClick = { }) {
-                Text(stringResource(R.string.button_1))
-            }
-            Button(modifier = buttonModifier, onClick = { }) {
-                Text(stringResource(R.string.button_2))
-            }
+        UserTextInput(id, name, age, height)
+        DataButtons(
+            id = id.intValue, name = name.value, age = age.intValue, height = height.doubleValue
+        )
+    }
+}
+
+@Composable
+fun UserTextInput(
+    id: MutableIntState,
+    name: MutableState<String>,
+    age: MutableIntState,
+    height: MutableDoubleState
+) {
+    var idText by remember { mutableStateOf("") }
+    var ageText by remember { mutableStateOf("") }
+    var heightText by remember { mutableStateOf("") }
+    var idError by remember { mutableStateOf(false) }
+    var ageError by remember { mutableStateOf(false) }
+    var heightError by remember { mutableStateOf(false) }
+    val textModifier = Modifier
+        .padding(8.dp)
+        .height(72.dp)
+        .fillMaxWidth()
+    TextField(modifier = textModifier, value = idText, onValueChange = {
+        idText = it
+        idError = !idText.isDigitsOnly()
+        id.intValue = idText.toIntOrNull() ?: 0
+    }, label = { Text("ID") })
+    TextField(modifier = textModifier, value = name.value, isError = idError, supportingText = {
+        if (idError) {
+            Text("请输入正确的ID")
+        }
+    }, onValueChange = {
+        name.value = it
+    }, label = { Text("Name") })
+    TextField(modifier = textModifier, value = ageText, isError = ageError, supportingText = {
+        if (ageError) {
+            Text("请输入正确的年龄")
+        }
+    }, onValueChange = {
+        ageText = it
+        ageError = !ageText.isDigitsOnly()
+        age.intValue = ageText.toIntOrNull() ?: 0
+    }, label = { Text("Age") })
+    TextField(modifier = textModifier, value = heightText, isError = heightError, supportingText = {
+        if (heightError) {
+            Text("请输入正确的身高")
+        }
+    }, onValueChange = {
+        heightText = it
+        heightError = !heightText.isDigitsOnly()
+        height.doubleValue = heightText.toDoubleOrNull() ?: 0.0
+    }, label = { Text("Height") })
+}
+
+@Composable
+fun DataButtons(
+    id: Int, name: String, age: Int, height: Double
+) {
+    Row(
+        horizontalArrangement = Arrangement.Center
+    ) {
+        val buttonModifier = Modifier
+            .padding(16.dp)
+            .width(128.dp)
+            .height(54.dp)
+        Button(modifier = buttonModifier, onClick = { }) {
+            Text(stringResource(R.string.button_1))
+        }
+        Button(modifier = buttonModifier, onClick = { }) {
+            Text(stringResource(R.string.button_2))
         }
     }
 }
