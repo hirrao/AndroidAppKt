526893066a3e949fb9ab5dc8e547b5ea690297c2
hirrao <git@hirrao.com>
2025-04-15 14:08:10 +0800

    提取出Dialog

--- app/src/main/java/com/hirrao/appktp/display/Dialogs.kt
+++ app/src/main/java/com/hirrao/appktp/display/Dialogs.kt
@@ -3,6 +3,7 @@ package com.hirrao.appktp.display
 import android.content.res.Configuration.UI_MODE_NIGHT_NO
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.content.res.Configuration.UI_MODE_TYPE_NORMAL
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -19,6 +20,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -36,63 +38,51 @@ import kotlinx.coroutines.launch
 fun UserDialog(
     user: User, showDialog: MutableState<Boolean>, dialogState: MutableState<DialogDisplayEnums>
 ) {
-    BasicAlertDialog(onDismissRequest = {
+    val text = "${stringResource(R.string.process_dialog_description_1)}\n" + "${
+        stringResource(
+            R.string.id_name
+        )
+    } : ${user.id}\n" + "${
+        stringResource(
+            R.string.name_name
+        )
+    } : ${user.name}\n" + "${
+        stringResource(
+            R.string.age_name
+        )
+    } : ${user.age}\n" + "${
+        stringResource(
+            R.string.height_name
+        )
+    } : ${user.height}"
+    CommonDialog(modifier = Modifier, onDismissRequest = {
         showDialog.value = false
-    }) {
+    }, text = text) {
         val buttonModifier = Modifier.padding(4.dp)
-        Surface(
-            modifier = Modifier
-                .wrapContentWidth()
-                .wrapContentHeight()
-        ) {
-            Column(modifier = Modifier.padding(16.dp)) {
-                Text(
-                    "${stringResource(R.string.process_dialog_description_1)}\n" + "${
-                        stringResource(
-                            R.string.id_name
-                        )
-                    } : ${user.id}\n" + "${
-                        stringResource(
-                            R.string.name_name
-                        )
-                    } : ${user.name}\n" + "${
-                        stringResource(
-                            R.string.age_name
-                        )
-                    } : ${user.age}\n" + "${
-                        stringResource(
-                            R.string.height_name
-                        )
-                    } : ${user.height}"
-                )
-                Spacer(modifier = Modifier.height(32.dp))
-                Row(modifier = Modifier.padding(2.dp)) {
-                    TextButton(
-                        onClick = {
-                            showDialog.value = false
-                            // @TODO: 检查是否成功, 错误处理
-                            CoroutineScope(Dispatchers.IO).launch {
-                                val selectUser = userDao.getById(user.id)
-                                if (selectUser == null) {
-                                    userDao.insert(user)
-                                    dialogState.value = DialogDisplayEnums.INSERT_SUCCESS
-                                } else dialogState.value = DialogDisplayEnums.INSERT_ERROR
-                            }
-                        }, modifier = buttonModifier
-                    ) {
-                        Text(stringResource(R.string.process_dialog_button_1))
+        Row(modifier = Modifier.padding(2.dp)) {
+            TextButton(
+                onClick = {
+                    showDialog.value = false
+                    CoroutineScope(Dispatchers.IO).launch {
+                        val selectUser = userDao.getById(user.id)
+                        if (selectUser == null) {
+                            userDao.insert(user)
+                            dialogState.value = DialogDisplayEnums.INSERT_SUCCESS
+                        } else dialogState.value = DialogDisplayEnums.INSERT_ERROR
                     }
-                    TextButton(
-                        onClick = { showDialog.value = false }, modifier = buttonModifier
-                    ) {
-                        Text(stringResource(R.string.process_dialog_button_2))
-                    }
-                    TextButton(
-                        onClick = { showDialog.value = false }, modifier = buttonModifier
-                    ) {
-                        Text(stringResource(R.string.process_dialog_button_3))
-                    }
-                }
+                }, modifier = buttonModifier
+            ) {
+                Text(stringResource(R.string.process_dialog_button_1))
+            }
+            TextButton(
+                onClick = { showDialog.value = false }, modifier = buttonModifier
+            ) {
+                Text(stringResource(R.string.process_dialog_button_2))
+            }
+            TextButton(
+                onClick = { showDialog.value = false }, modifier = buttonModifier
+            ) {
+                Text(stringResource(R.string.process_dialog_button_3))
             }
         }
     }
@@ -101,23 +91,42 @@ fun UserDialog(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ResultDialog(dialogState: MutableState<DialogDisplayEnums>) {
-    BasicAlertDialog(onDismissRequest = {
-        dialogState.value = DialogDisplayEnums.NONE
-    }) {
+    CommonDialog(
+        onDismissRequest = { dialogState.value = DialogDisplayEnums.NONE },
+        modifier = Modifier,
+        text = "Empty"
+    ) {
         val buttonModifier = Modifier.padding(4.dp)
-        Surface(
-            modifier = Modifier
-                .wrapContentWidth()
-                .wrapContentHeight()
-        ) {
-            Column(modifier = Modifier.padding(16.dp)) {
-                Text("EMPTY")
-                Spacer(modifier = Modifier.height(32.dp))
-                Row(modifier = Modifier.padding(2.dp)) {
-                    TextButton(
-                        modifier = buttonModifier,
-                        onClick = { dialogState.value = DialogDisplayEnums.NONE }) {
-                        Text("Dismiss")
+        Row(modifier = Modifier.padding(2.dp)) {
+            TextButton(
+                modifier = buttonModifier,
+                onClick = { dialogState.value = DialogDisplayEnums.NONE }) {
+                Text("Dismiss")
+            }
+        }
+    }
+}
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun CommonDialog(
+    modifier: Modifier,
+    onDismissRequest: () -> Unit,
+    text: String,
+    buttons: @Composable () -> Unit,
+) {
+    Box(modifier = Modifier.then(modifier)) {
+        BasicAlertDialog(onDismissRequest = onDismissRequest, modifier = Modifier) {
+            Surface(
+                modifier = Modifier
+                    .wrapContentWidth()
+                    .wrapContentHeight()
+            ) {
+                Column(modifier = Modifier.padding(16.dp)) {
+                    Text(text)
+                    Spacer(modifier = Modifier.height(32.dp))
+                    Row(modifier = Modifier.padding(2.dp), verticalAlignment = Alignment.Bottom) {
+                        buttons()
                     }
                 }
             }
